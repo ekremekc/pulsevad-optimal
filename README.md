563115ab6082605709987c26d9efe77f389f196d
ekremekc <ekrem.ekici70@hotmail.com>
2025-03-11 09:58:51 +0300

    very simple example is done

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ This library can be installed directly with conda. We can make a conda environme
 ```bash
 conda create -n pulsevad python=3.13.0
 conda activate pulsevad
-conda install pip
+conda install pip Jinja2
 pip3 install pandas numpy seaborn matplotlib
 conda install -c conda-forge scikit-learn
 ```
---
+++ scripts/linear_regression.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Sample Data
+# Pellethane HVAD - Carbothane HVAD - HeartMate II - HeartMate 3 - HeartWare
+df = pd.DataFrame({
+    'Diameter': [4.8, 4.8, 6.0, 6.6, 6.0],
+    'Coating': ['Pellethane', 'Carbothane', 'soft silicone', 'polyurethane', 'polyurethane'],
+    'InfectionRate': [10.1, 9.1, 7.2, 11.9, 10.5]
+})
+
+# jacketing and dressing to be added 
+
+# One-Hot Encoding for Categorical Variable
+encoder = OneHotEncoder(sparse_output=False, drop='first')
+coating_encoded = encoder.fit_transform(df[['Coating']])
+coating_columns = encoder.get_feature_names_out(['Coating'])
+
+print(coating_encoded)
+
+
+# Merge Encoded Data
+df_encoded = pd.concat([df.drop(columns=['Coating']), pd.DataFrame(coating_encoded, columns=coating_columns)], axis=1)
+
+# Define Features and Target Variable
+X = df_encoded.drop(columns=['InfectionRate'])
+y = df_encoded['InfectionRate']
+
+# Set data
+regressor = LinearRegression().fit(X, y)
+
+# Train the Regression Model
+model = LinearRegression()
+model.fit(X, y)
+
+# Predictions
+y_pred = model.predict(X)
+
+# Model Evaluation
+mse = mean_squared_error(y, y_pred)
+r2 = r2_score(y, y_pred)
+
+# Print Results
+print("Model Coefficients:", dict(zip(X.columns, model.coef_)))
+print("Intercept:", model.intercept_)
+print("Mean Squared Error:", mse)
+print("R-Squared Value:", r2)
+
+# Visualization of Feature Importance
+plt.figure(figsize=(8, 5))
+sns.barplot(x=X.columns, y=model.coef_)
+plt.title("Feature Importance in Infection Rate Prediction")
+plt.xlabel("Wire Properties")
+plt.ylabel("Coefficient Value")
+plt.xticks(rotation=45)
+plt.show()
+
+df_predict = pd.DataFrame({
+    'Diameter': [4.8],
+    'Coating_Pellethane': [0],
+    'Coating_polyurethane': [1],
+    'Coating_soft silicone': [0],
+})
+
+print(model.predict(df_predict))
+
+corr = df_encoded.corr()
+corr.style.background_gradient(cmap='coolwarm')
